d65a7dcbb4284e414b7d8799d948958f89878289
AAAbdulaziz <ashurov635@gmail.com>
2020-12-13 22:58:02 +0500

    Second

--- btre/settings.py
+++ btre/settings.py
@@ -69,13 +69,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'abdulaziz16',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
